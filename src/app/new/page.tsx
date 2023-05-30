bb11629a478e5041be5f9d6e8c230351a3a85bc7
Gottfried Kleinberger <friedi.frieds@gmail.com>
2023-05-30 01:05:13 -0400

    wip

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function New() {
   return (
     <>
-      <h1 className="text-5xl pb-7">New</h1>
+      <h1 className="text-5xl pb-7">New Todo</h1>
       <Link href="/" className="text-lg hover:text-yellow-300">
         Home
       </Link>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,27 @@
 import "./globals.css";
 import Link from "next/link";
+import { prisma } from "@/db";
 
-export default function Home() {
+export default async function Home() {
+  const getTodos =  () => {
+      return  prisma.todo.findMany();
+    }
+
+  const todos = await getTodos();
+
+  const renderTodos = todos.map(todo => {
+    return <li key={todo.id}>{todo.title}</li>
+  })
+
   return (
     <>
       <header className="flex items-center justify-between">
-        <h1 className="text-5xl pb-7">Todos</h1>
-        <Link href="/new" className="text-lg hover:text-yellow-300">
+        <h1 className="text-5xl">Todos</h1>
+
+        <Link href="/new" className="px-2 py-1 mb-4 border rounded focus-within:bg-emerald-600 hover:bg-emerald-600 border-emerald-300">
           New
         </Link>
+
       </header>
     </>
   );
